8bb2b227e39285e7caea78d8a5d56459c06d52cc
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-08-19 19:06:55 +0300

    fix carousel size

--- src/components/CaroulelMovieItem/CaroulelMovieItem.tsx
+++ src/components/CaroulelMovieItem/CaroulelMovieItem.tsx
@@ -1,31 +1,34 @@
-import { RefObject } from 'react';
-import { Typography } from '@mui/material'
-import { MovieShortData } from '../../types/types'
-import { Link } from 'react-router';
-import styles from './CaroulelMovieItem.module.css'
+import { RefObject } from "react";
+import { Typography } from "@mui/material";
+import { MovieShortData } from "../../types/types";
+import { Link } from "react-router";
+import styles from "./CaroulelMovieItem.module.css";
 
 interface CaroulelMovieItemProps extends MovieShortData {
-	ref: RefObject<HTMLLIElement>
+  ref: RefObject<HTMLLIElement>;
 }
 
-export default function CaroulelMovieItem({ kinopoiskId, nameRu, posterUrl, ref }
-	: CaroulelMovieItemProps) {
-
-	return (
-		<li className={styles.movie} ref={ref}>
-			<Link to={'/movies/' + kinopoiskId}>
-				<div className={styles.poster}>
-					<img className={styles.image} src={posterUrl} alt="poster" />
-				</div>
-				<Typography
-					variant="h5"
-					component="h5"
-					align='center'
-					sx={{ fontWeight: 500, fontSize: 16, pt: 0.5 }}
-				>
-					{nameRu}
-				</Typography>
-			</Link>
-		</li>
-	)
+export default function CaroulelMovieItem({
+  kinopoiskId,
+  nameRu,
+  posterUrl,
+  ref,
+}: CaroulelMovieItemProps) {
+  return (
+    <li className={styles.movie} ref={ref}>
+      <Link to={"/movies/" + kinopoiskId}>
+        <div className={styles.poster}>
+          <img className={styles.image} src={posterUrl} alt="poster" />
+        </div>
+        <Typography
+          variant="h5"
+          component="h5"
+          align="center"
+          sx={{ fontWeight: 500, fontSize: 16, pt: 0.5 }}
+        >
+          {nameRu}
+        </Typography>
+      </Link>
+    </li>
+  );
 }
